46dca2dc05a5b046eddcbce94b5db44b3d55e41a
swamymanjunath <50077381+swamymanjunath@users.noreply.github.com>
2019-04-28 22:05:37 +0530

    Changes

--- src/page/LoginPage.java
+++ src/page/LoginPage.java
@@ -1,10 +1,14 @@
 package page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage
 {
+	@FindBy(name="username")
+	private WebElement un;
 	LoginPage(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
